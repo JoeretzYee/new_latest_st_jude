abd8c680be2c2be24b67fa3f9c4463c4aae76db3
Joeretz <56264637+JoeretzYee@users.noreply.github.com>
2022-02-28 13:36:03 +0800

    Add files via upload

--- api/views.py
+++ api/views.py
@@ -3,7 +3,20 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import PatientSerializer, TreatmentSerializer, PatientAppointmentSerializer, ProcessPaymentsSerializer
 from .models import Patient, Treatment, PatientAppointment, ProcessPayments
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
 
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
 
 class PatientViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
