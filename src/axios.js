4809556ab6ea3e0e9b01a0420731f07714dfa042
Joeretz <yee.it111@gmail.com>
2022-03-02 13:51:24 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-// axios.defaults.xsrfCookieName = "csrftoken";
-
-
-
 const instance = axios.create({
     baseURL: "http://127.0.0.1:8000",
     headers: {
