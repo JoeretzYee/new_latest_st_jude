cd1896e3af07e0b27338f75d4e2ac273124bf885
Joeretz <yee.it111@gmail.com>
2022-03-02 11:58:39 +0800

    adding cors allowed origins

--- core/settings.py
+++ core/settings.py
@@ -100,8 +100,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://st-jude-management-system.herokuapp.com/",
+   
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
